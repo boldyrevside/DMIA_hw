005aa5eab2843d64bedbe523f1945af589ad7263
boldyrevside <boldyrevside@gmail.com>
2016-10-21 11:26:24 +0300

    FizzBuzz task

--- python_hw.py
+++ python_hw.py
@@ -1,1 +1,21 @@
 
+"""
+Python
+"""
+
+
+def FizzBuzz():
+    a=range(1,101)
+    i=0
+    while i<100:
+        if a[i]%3==0 and a[i]%5==0:
+           a[i]= "FizzBuzz"
+        elif a[i]%3==0:
+            a[i]="Fizz"
+        else:
+            if a[i]%5==0:
+                a[i]="Buzz"
+        i=i+1
+    print a                   
+
+FizzBuzz()                   
